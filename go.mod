de7d3d621dab04615b9d8ec7a97991d53fe68439
tjweldon <tomd90@gmail.com>
2022-06-25 17:22:33 +0100

    animated histogram poc working

--- go.mod
+++ go.mod
@@ -9,8 +9,17 @@ require (
 )
 
 require (
+	gioui.org v0.0.0-20210729070555-8cec7e04eb71 // indirect
+	gioui.org/cpu v0.0.0-20210727122813-41509bcd3462 // indirect
 	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/go-p5/p5 v0.13.0 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/gonum v0.9.2 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"bit-wordy/src/primitives"
 	"fmt"
 	"github.com/alexflint/go-arg"
+	"github.com/go-p5/p5"
+	"image/color"
 	"log"
 	"time"
 )
@@ -87,13 +89,104 @@ func (i Iterate) Run(p *cached.Patterns) (err error) {
 	return err
 }
 
+type Axis struct {
+	Scale      float64
+	Start, End pair[float64]
+}
+
+func AxisX(start pair[float64], length, scale float64) Axis {
+	end := pair[float64]{start[0] + length, start[1]}
+	return Axis{
+		Scale: scale,
+		Start: start,
+		End:   end,
+	}
+}
+
+func AxisY(start pair[float64], length, scale float64) Axis {
+	end := pair[float64]{start[0], start[1] - length}
+	return Axis{
+		Scale: scale,
+		Start: start,
+		End:   end,
+	}
+}
+
+func (a Axis) Draw() {
+	p5.StrokeWidth(2)
+	p5.Stroke(color.Black)
+	x1, y1 := a.Start.unpack()
+	x2, y2 := a.End.unpack()
+	p5.Line(x1, y1, x2, y2)
+}
+
+type Renderer struct {
+	Angle        float64
+	W, H         int
+	Margin       float64
+	XAxis, YAxis Axis
+	Hist         []int
+}
+
+func (r *Renderer) Setup() {
+	p5.Canvas(r.W, r.H)
+	p5.Background(color.Gray{Y: 220})
+}
+
+func (r *Renderer) Draw() {
+	r.drawAxes()
+	i := time.Now().UnixMilli() % 10
+	r.Hist[i]++
+	r.drawHist()
+}
+
+func (r *Renderer) drawAxes() {
+	p5.StrokeWidth(2)
+	p5.Stroke(color.Black)
+	r.XAxis.Draw()
+	r.YAxis.Draw()
+}
+
+func (r Renderer) drawHist() {
+	btm := float64(r.H) - r.Margin
+	left := r.Margin
+	max := 0.0
+	for _, n := range r.Hist {
+		if float64(n) >= max {
+			max = float64(n)
+		}
+	}
+
+	p5.StrokeWidth(0)
+	for i, n := range r.Hist {
+		p5.Fill(color.RGBA{R: 0, G: 175, B: 50, A: 255})
+		barHeight := float64(r.H) - 2*r.Margin
+		p5.Rect(left+float64(i)*100.0, btm, 99.0, -(barHeight*float64(n))/max)
+	}
+}
+
+type Visual struct {
+	Times int `arg:"positional"`
+}
+
+func (v Visual) Run() {
+	r := &Renderer{
+		W: 1800, H: 600, Margin: 40, Hist: make([]int, 10),
+	}
+	r.XAxis = AxisX(pair[float64]{r.Margin, float64(r.H) - r.Margin}, float64(r.W)-2.0*r.Margin, 1)
+	r.YAxis = AxisY(pair[float64]{r.Margin, float64(r.H) - r.Margin}, float64(r.H)-2.0*r.Margin, 1)
+	p5.Run(r.Setup, r.Draw)
+	fmt.Println("VISUAL")
+}
+
 var args struct {
-	Build bool     `arg:"-b,--build"`
-	Dump  bool     `arg:"-d,--dump"`
-	Load  bool     `arg:"-l,--load"`
-	Play  bool     `arg:"-p,--play"`
-	Guess *Guess   `arg:"subcommand:guess"`
-	Iter  *Iterate `arg:"subcommand:iter"`
+	Build  bool     `arg:"-b,--build"`
+	Dump   bool     `arg:"-d,--dump"`
+	Load   bool     `arg:"-l,--load"`
+	Play   bool     `arg:"-p,--play"`
+	Guess  *Guess   `arg:"subcommand:guess"`
+	Iter   *Iterate `arg:"subcommand:iter"`
+	Visual *Visual  `arg:"subcommand:visual"`
 }
 
 func main() {
@@ -144,6 +237,9 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+	if args.Visual != nil {
+		args.Visual.Run()
 	}
 
 	fmt.Println("Done!")
